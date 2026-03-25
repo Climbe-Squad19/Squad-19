33487944c6c701dd2d0663ae795506172ed3f10c
Pedro Faria <168576911+PedroFariaBarreto@users.noreply.github.com>
2026-03-25 20:31:36 -0300

    Ajuste na Classe Reunião

--- gestao-contratos/src/main/java/br/com/residencia/gestao_contratos/classes/Reuniao.java
+++ gestao-contratos/src/main/java/br/com/residencia/gestao_contratos/classes/Reuniao.java
@@ -1,8 +1,21 @@
 package br.com.residencia.gestao_contratos.classes;
 
-import jakarta.persistence.*;
-import lombok.Data;
 import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Data
 @Entity
@@ -13,15 +26,32 @@ public class Reuniao {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String pauta; 
+    private String pauta;
 
     @ManyToOne
-    private Empresa empresa; 
+    private Empresa empresa;
 
-    private LocalDateTime dataHora; 
-    
-    private boolean presencial; // Checkbox pra decidir se é presencial ou online
-    
-    private String linkOnline; // caso seja online
-    private String sala; // caso seja presencial
+    @ManyToOne
+    private Contrato contrato;    
+
+    private LocalDateTime dataHora;
+    private boolean presencial;
+    private String linkOnline;
+    private String sala;
+
+    @Enumerated(EnumType.STRING)
+    private StatusReuniao status;     
+    @ElementCollection
+    @CollectionTable(
+        name = "reuniao_participantes",
+        joinColumns = @JoinColumn(name = "reuniao_id")
+    )
+    private List<Long> participantesIds; 
+
+    @Column(updatable = false)
+    private LocalDateTime dataCriacao; 
+
+    public enum StatusReuniao {
+        AGENDADA, CANCELADA, REALIZADA
+    }
 }
