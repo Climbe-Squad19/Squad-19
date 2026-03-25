505d4797ebc2252a4f0c7a481fcf61dde6b500c9
Pedro Faria <168576911+PedroFariaBarreto@users.noreply.github.com>
2026-03-25 20:29:39 -0300

    Ajuste na Classe Contrato

--- gestao-contratos/src/main/java/br/com/residencia/gestao_contratos/classes/Contrato.java
+++ gestao-contratos/src/main/java/br/com/residencia/gestao_contratos/classes/Contrato.java
@@ -1,14 +1,25 @@
 package br.com.residencia.gestao_contratos.classes;
 
-
-import jakarta.persistence.*;
-import lombok.Data;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "contratos")
 public class Contrato {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,10 +30,25 @@ public class Contrato {
     @ManyToOne
     private Empresa empresa;
 
+    @ManyToOne
+    private Usuario usuarioResponsavel;  
+
+    private String tipoServico;         
+
+    @Enumerated(EnumType.STRING)
+    private StatusContrato status;      
+
     private LocalDate dataInicio;
     private LocalDate dataFim;
-    private String linkContratoAssinado; // PDF no Google Drive
-    
+    private Integer diasAvisoVencimento; 
+    private String linkContratoAssinado;
     private boolean renovacaoAutomatica;
     private String observacoes;
+
+    @Column(updatable = false)
+    private LocalDateTime dataCriacao;   
+
+    public enum StatusContrato {
+        ATIVO, ENCERRADO, CANCELADO
+    }
 }
