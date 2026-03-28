c87e1e5cdabe76cad2644d7574c39aab7c3c9726
PedroFariaB <pedrofaria150506@gmail.com>
2026-03-28 01:13:47 -0300

    modificação no dto response reunião

--- gestao-contratos/src/main/java/br/com/residencia/gestao_contratos/dtos/response/ReuniaoResponse.java
+++ gestao-contratos/src/main/java/br/com/residencia/gestao_contratos/dtos/response/ReuniaoResponse.java
@@ -1,10 +1,12 @@
 package br.com.residencia.gestao_contratos.dtos.response;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 import java.time.LocalDateTime;
 import java.util.List;
+
+import br.com.residencia.gestao_contratos.classes.Reuniao;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
@@ -15,11 +17,12 @@ public class ReuniaoResponse {
     private Long empresaId;
     private String nomeEmpresa;
     private Long contratoId;
+    private Reuniao.TipoReuniao tipo;   
     private LocalDateTime dataHora;
     private boolean presencial;
     private String linkOnline;
     private String sala;
-    private String status;  
+    private String status;
     private List<Long> participantesIds;
     private LocalDateTime dataCriacao;
 }
