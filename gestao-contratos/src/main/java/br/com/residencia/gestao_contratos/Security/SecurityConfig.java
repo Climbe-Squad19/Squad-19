c8b86021cbbf556274c5a14c6a07e615410e5daa
Pedro Faria Barreto <pedrofaria150506@gmail.com>
2026-04-22 15:35:48 -0300

    reajuste no passwordencoder do security config

--- gestao-contratos/src/main/java/br/com/residencia/gestao_contratos/Security/SecurityConfig.java
+++ gestao-contratos/src/main/java/br/com/residencia/gestao_contratos/Security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
