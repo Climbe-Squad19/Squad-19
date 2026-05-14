ba283f0316ebca5f6a9332f0a5a62f05a8e09427
Pedro Faria Barreto <pedrofaria150506@gmail.com>
2026-05-14 09:33:19 -0300

    ajuste no security config

--- gestao-contratos/src/main/java/br/com/residencia/gestao_contratos/Security/SecurityConfig.java
+++ gestao-contratos/src/main/java/br/com/residencia/gestao_contratos/Security/SecurityConfig.java
@@ -65,7 +65,7 @@ public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(auth -> auth
             .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-            .requestMatchers(HttpMethod.GET, "/documentos/*/download").permitAll()
+            .requestMatchers("/documentos/**").permitAll()
             .requestMatchers("/auth/**").permitAll()
             .requestMatchers("/integracoes/google/callback").permitAll()
             .requestMatchers("/actuator/health", "/actuator/info").permitAll()
